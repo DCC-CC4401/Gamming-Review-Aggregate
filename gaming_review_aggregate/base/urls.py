83f86db9a3dfd56b15431c1eedf8970c962a8ad7
ophelaide <matias.seda@ug.uchile.cl>
2021-04-29 18:35:00 -0300

    Prueba

--- gaming_review_aggregate/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('home', views.home, name='home'),
-  path('login', views.login, name='login'),
-  path('popular_games', views.popular_games, name='popular_games'),
-]
