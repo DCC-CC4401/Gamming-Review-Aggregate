cf5b429f36fd2e69fc19f43f8baeec01c8bf5d85
Felipe Leal <pipelealcerro@gmail.com>
2021-04-29 17:01:53 -0400

    Añadiendo archivos html y css

--- gaming_review_aggregate/base/urls.py
+++ gaming_review_aggregate/base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('home', views.home, name='home'),
+  path('login', views.login, name='login'),
+  path('popular_games', views.popular_games, name='popular_games'),
 ]
--- gaming_review_aggregate/base/views.py
+++ gaming_review_aggregate/base/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def home(request): #the homepage view
     if request.method == "GET":
        return render(request, "base/index.html")
+
+def login(request): #the login view
+    if request.method == "GET":
+       return render(request, "base/login.html")
+
+def popular_games(request): #the login view
+    if request.method == "GET":
+       return render(request, "base/popular-games.html")
