16250881453fb4f9b7ea737f7de1b0f80441a264
ophelaide <matias.seda@ug.uchile.cl>
2021-05-01 14:30:45 -0300

    Creación listas python con plataformas de juegos y tipos de juegos

---
+++ selects/plataforms.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 
+
+plataforms = ["Android", "Arcade", "Atari", 
+        "Game Boy", "Game Boy Advance", "Game Boy Color", 
+        "Nintendo 3DS", "Nintendo 64", "Nintendo DS", "Nintendo DSi", 
+        "Nintendo Entertainment System (NES)", "Nintendo GameCube", 
+        "Nintendo Switch", "PC", "PlayStation", "PlayStation 2", "PlayStation 3",
+        "PlayStation 4", "PlayStation 5", "PlayStation Portable (PSP)", "Sega", 
+        "Super Nintendo Entertainment System (SNES)", "Wii", "Wii U", 
+        "Xbox", "Xbox 360", "Xbox One", "Xbox Series", "Other"]
+
+genres = ["Action", "Adventure", "Fighting", "Platform",
+        "Puzzle", "Racing", "Role-playing", "Shooter", "Simulation",
+        "Sports", "Strategy", "Other"]
+
+
+
