46254862fe4ac7835d3beeb60904aca3e58edd66
Felipe Leal <pipelealcerro@gmail.com>
2021-05-01 15:46:10 -0400

    Avance login

--- gaming_review_aggregate/base/models.py
+++ gaming_review_aggregate/base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30, default="user")
     password = models.CharField(max_length=30)
 
 #class Game(models.Model):
