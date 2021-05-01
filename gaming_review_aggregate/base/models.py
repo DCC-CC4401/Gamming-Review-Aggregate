b441542c3b2b9ae5fb2a309787799a3ddc38f079
Alex Walmsley <akex.walmsley@gmail.com>
2021-05-01 15:00:35 -0400

    Cambios iniciales para adoptar PostgreSQL

--- gaming_review_aggregate/base/models.py
+++ gaming_review_aggregate/base/models.py
@@ -4,3 +4,20 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     nombre = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+
+#class Game(models.Model):
+   # nombre = models.CharField(max_length=150)
+
+#class Developer(models.Model):
+   # nombre = models.CharField(max_length=150)
+
+#class Rating(models.Model):
+ #   puntaje = models.IntegerField(max_value=10, min_value=0) # esta wea la inventé
+
+#class Review(models.Model):
+   # body= models.CharField(max_length=5000)
+
+#class Genre(models.Model):
+    #nombre = models.CharField(max_length=30)
+
+
--- gaming_review_aggregate/gaming_review_aggregate/settings.py
+++ gaming_review_aggregate/gaming_review_aggregate/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gaming_review_aggregate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gra',
+        'USER': 'ing1',
+        'PASSWORD': 'graing1',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
@@ -87,7 +91,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-#AUTH_USER_MODEL = 'base.User'
+AUTH_USER_MODEL = 'base.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
