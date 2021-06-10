155841b9ca5457cc92518ee6e9462f45d174c5a4
Alex Walmsley <akex.walmsley@gmail.com>
2021-06-10 18:04:13 -0400

    Actualizando modelos (rating, platforms, amix etc)

--- gaming_review_aggregate/base/models.py
+++ gaming_review_aggregate/base/models.py
@@ -1,32 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-   nombre = models.CharField(max_length=30, default="user")
-   password = models.CharField(blank=False,max_length=100)
-   edad = models.IntegerField(null=True)
-   descripcion = models.TextField(max_length=2000, default="Acá va la descripción de usuario")
+    nombre = models.CharField(max_length=30, default="user")
+    password = models.CharField(blank=False, max_length=100)
+    edad = models.IntegerField(null=True)
+    descripcion = models.TextField(max_length=2000, default="Acá va la descripción de usuario")
+
 
 class Game(models.Model):
-   nombre = models.CharField(max_length=150)
-   anio = models.IntegerField()
-   descripcion = models.TextField(max_length=2000)
-   desarrollador = models.CharField(max_length=150)
-   plataforma = models.CharField(max_length=150)
-   genero = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=150)
+    anio = models.IntegerField()
+    descripcion = models.TextField(max_length=2000)
+    desarrollador = models.CharField(max_length=150)
+    genero = models.CharField(max_length=150)
 
-#class Developer(models.Model):
-   # nombre = models.CharField(max_length=150)
 
-#class Rating(models.Model):
- #   puntaje = models.IntegerField(max_value=10, min_value=0) # esta wea la inventé
+class Platform(models.Model):
+    name = models.CharField(max_length=150)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    score = models.IntegerField(max_value=10, min_value=0)  # nose si existen min y max value
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    review_body = models.TextField(max_length=2000)
+
+
+class Friendship(models.Model):
+   user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+   user2 = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Review(models.Model):
-   author = models.ForeignKey(User, on_delete=models.CASCADE)
-   game = models.ForeignKey(Game, on_delete=models.CASCADE)
-   body = models.TextField(max_length=2000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    body = models.TextField(max_length= 2000)
 
-#class Genre(models.Model):
-    #nombre = models.CharField(max_length=30)
+#class GameMedia(models.Model):
+   #img = models.image??? ayuda
+   #game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+#class UserMedia(models.Model):
+  #user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
